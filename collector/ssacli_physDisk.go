bd06a41d4136f389207db06c8ccce237ad1c5860
jdd272 <82543283+jdd272@users.noreply.github.com>
2021-06-16 16:26:32 +0200

    Update ssacli_physDisk.go

--- collector/ssacli_physDisk.go
+++ collector/ssacli_physDisk.go
@@ -74,7 +74,7 @@ func (c *SsacliPhysDiskCollector) Describe(ch chan<- *prometheus.Desc) {
 // Handle error
 func (c *SsacliPhysDiskCollector) Collect(ch chan<- prometheus.Metric) {
 	if desc, err := c.collect(ch); err != nil {
-		log.Debugln("[ERROR] failed collecting metric %v: %v", desc, err)
+		promlog.Debugln("[ERROR] failed collecting metric %v: %v", desc, err)
 		ch <- prometheus.NewInvalidMetric(desc, err)
 		return
 	}
@@ -89,14 +89,14 @@ func (c *SsacliPhysDiskCollector) collect(ch chan<- prometheus.Metric) (*prometh
 	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
 
 	if err != nil {
-		log.Debugln("[ERROR] smart log: \n%s\n", out)
+		promlog.Debugln("[ERROR] smart log: \n%s\n", out)
 		return nil, err
 	}
 
 	data := parser.ParseSsacliPhysDisk(string(out))
 
 	if data == nil {
-		log.Fatal("Unable get data from ssacli sumarry exporter")
+		promlog.Fatal("Unable get data from ssacli sumarry exporter")
 		return nil, nil
 	}
 
