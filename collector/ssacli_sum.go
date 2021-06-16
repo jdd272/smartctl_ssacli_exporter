685c80ab8389f13a1f8553792bcaaae715a431bd
jdd272 <82543283+jdd272@users.noreply.github.com>
2021-06-16 16:27:02 +0200

    Update ssacli_sum.go

--- collector/ssacli_sum.go
+++ collector/ssacli_sum.go
@@ -102,7 +102,7 @@ func (c *SsacliSumCollector) Describe(ch chan<- *prometheus.Desc) {
 // Handle error
 func (c *SsacliSumCollector) Collect(ch chan<- prometheus.Metric) {
 	if desc, err := c.collect(ch); err != nil {
-		log.Debugln("[ERROR] failed collecting metric %v: %v", desc, err)
+		promlog.Debugln("[ERROR] failed collecting metric %v: %v", desc, err)
 		ch <- prometheus.NewInvalidMetric(desc, err)
 		return
 	}
@@ -113,14 +113,14 @@ func (c *SsacliSumCollector) collect(ch chan<- prometheus.Metric) (*prometheus.D
 	out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
 
 	if err != nil {
-		log.Debugln("[ERROR] smart log: \n%s\n", out)
+		promlog.Debugln("[ERROR] smart log: \n%s\n", out)
 		return nil, err
 	}
 
 	data := parser.ParseSsacliSum(string(out))
 
 	if data == nil {
-		log.Fatal("Unable get data from ssacli sumarry exporter")
+		promlog.Fatal("Unable get data from ssacli sumarry exporter")
 		return nil, nil
 	}
 
